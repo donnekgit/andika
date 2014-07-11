1ef87dd7496c76301caea1deab0b1dc088a0c48c
donnek <kevin@dotmon.com>
2014-07-11 20:14:21 +0100

    Start on handling prose

---
+++ poetry/convert_prose.php
@@ -0,0 +1,166 @@
+<?php
+
+/* 
+*********************************************************************
+Copyright Kevin Donnelly 2012-2014.
+kevindonnelly.org.uk
+This file is part of Andika!, a set of tools for writing Swhili in Arbic script..
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License or the GNU
+Affero General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+and the GNU Affero General Public License along with this program.
+If not, see <http://www.gnu.org/licenses/>.
+*********************************************************************
+*/
+
+/*
+This script converts Swahili poems in Arabic script into Roman script, and vice versa, adding an automatically
+generated transcription.  For more information, see includes/optionhandling.php, or type: php poetry/import_swa.php -h
+*/
+
+// Initialise required stuff.
+include("./andika/config.php");
+include("./includes/fns.php");
+//include("./includes/optionhandling.php");
+require_once 'poetry/QueryPath-2.1.2-minimal/QueryPath.php';
+
+$shortopts="i:v:o::l::s::th";  // short options are a string
+$longopts=array("input:", "vipande:", "output::", "layout::", "script::", "transcription", "help");  // long options are an array
+$options = getopt($shortopts, $longopts);  
+if (isset($options["i"]))
+{
+    $input=explode(".", $options["i"]);
+}
+elseif (isset($options["input"]))
+{
+    $input=explode(".", $options["input"]);
+}
+if ($options["o"]=="pdf" or $options["output"]=="pdf")
+{
+    $output="pdf";
+    if (empty($options["l"]) or empty($options["layout"]))
+    {
+        $layout="vip-space";
+        $columns="rrl";
+    }
+}
+$poem=$input[0];
+$type=$input[1];
+
+// Debug: check options and variables.
+//print_r($options)."\n";
+//echo $input."\n";
+echo $poem."\n";
+echo $type."\n";
+echo $vipande_no."\n";
+echo $output."\n";
+echo $layout."\n";
+echo $columns."\n";
+echo $script."\n";
+
+// Locate the input file, depending on extension.
+if ($type=="odt")
+{
+    $file="zip://poetry/inputs/".$poem.".".$type."#content.xml";
+}
+elseif ($type=="txt")
+{
+    $file="poetry/inputs/".$poem.".".$type;
+}
+
+// Set up a dir for each poem's output.
+exec("mkdir -p poetry/outputs/".$poem);
+
+/*
+// For database import, set up a table for each poem.
+if ($output=="db")
+{
+    include("poetry/create_poemlines.php");
+}
+*/
+
+/*
+// Set up stanza furniture.
+$stanza_no=1;  // start stanza counter
+$stanza_contents="";  // set up a holder for the contents of the whole stanza
+$vipande=array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p');  // letters to signify location of the kipande in the stanza
+// Truncate the array of vipande letters to the total number of vipande in the stanza.  (NOTE: if you put $vipande= at the front of the line, $vipande will contain the cut pieces, not the ones you want to keep!)
+array_splice($vipande, $vipande_no);
+// print_r($vipande);
+$first_half=array('a', 'c', 'e', 'g', 'i', 'k', 'm', 'o');  // vipande we want to have at the beginning of a line
+*/
+
+// Read the lines in the file into an array, based on extension.
+if ($type=="odt")
+{
+    foreach (qp($file)->find('text|p') as $line) 
+    {
+        $poemlines[]=$line->text();
+    }
+}
+elseif ($type=="txt")
+{    
+    $poemlines=file($file);
+}
+
+// Open the output file.
+if ($output=="pdf")
+{
+    include("tex/prose_pdf_open.php");  // Open the tex file.
+}
+elseif ($output=="txt")
+{
+    $fp=fopen("poetry/outputs/$poem/{$poem}.txt", "w") or die("Can't create the file");
+}
+
+$stanza_no=1;
+
+// Read each line of the array and handle it.
+foreach ($poemlines as $poemline)
+{
+    $translit=ar2rom(trim($poemline)); // transliterate
+    $standard=trim(standardise($translit));  // apply standard transcription
+    $roman=trim(close_trans($translit));  // apply close phonetic transcription
+    
+    if ($output=="pdf")
+    {
+        fwrite($fp, "\\textarabic{".$poemline."} & ");
+        fwrite($fp, "\\textarabic{".convert_numbers($stanza_no)."} \\\\* \n");  // * disallows a pagebreak after this row
+        fwrite($fp, "\T{".$roman."} & \T{".$stanza_no."}\\\\* \n");
+        //fwrite($fp, "\S{".$standard."} & \S{".$stanza_no."} \\\\ \n");
+    }
+    elseif ($output=="txt")
+    {
+        fwrite($fp, $poemline."\n");
+        fwrite($fp, $roman."\n");
+        //fwrite($fp, $standard."\n");
+//         if (isset($options["t"]))
+//         {
+//             fwrite($fp, $standard."\n");
+//         }
+    }
+        
+    $stanza_no++;  // increment the stanza number
+}
+
+// Close off the output, depending on extension.
+if ($output=="pdf")
+{
+    include("tex/pdf_close.php");
+}
+elseif ($output=="txt")
+{
+    fclose($fp);
+}
+
+?>
--- poetry/tex/prose_pdf_open.php
+++ poetry/tex/prose_pdf_open.php
@@ -8,6 +8,7 @@ $header=file_get_contents("poetry/tex/poem_header.tex");
 fwrite($fp, $header);
 fwrite($fp, "\n");
 
+/*
 if (file_exists("poetry/tex/{$poem}_title.tex"))  // Call a title file specific to this poem if it exists ....
 {
     $title=file_get_contents("poetry/tex/{$poem}_title.tex");
@@ -21,12 +22,12 @@ else  // ... otherwise, just use the name of the poem as the title.
 }
 fwrite($fp, $title);
 fwrite($fp, "\n");
-
+*/
 
 //fwrite($fp, "\begin{longtable}{{$columns}} \n\n");
 fwrite($fp, "\begin{longtable}{{rl}} \n\n");
 
-fwrite($fp, "\makebox[8cm][r]{} & & \makebox[8cm][r]{} \\\\ \n\n");  // Dummy row to force shrinkage of the space between the two vipande columns by reserving space for the first column.  The spacer for the third column has only minimal effect, but moves the numbers column over a bit to the left.  This is required only because of the multicolumn (columns 1 and 2) span of the English translation, but it doesn't seem to cause problems for convert.php, so let's keep it as default until proven otherwise.
+//fwrite($fp, "\makebox[8cm][r]{} & & \makebox[8cm][r]{} \\\\ \n\n");  // Dummy row to force shrinkage of the space between the two vipande columns by reserving space for the first column.  The spacer for the third column has only minimal effect, but moves the numbers column over a bit to the left.  This is required only because of the multicolumn (columns 1 and 2) span of the English translation, but it doesn't seem to cause problems for convert.php, so let's keep it as default until proven otherwise.
 
 
 ?>
